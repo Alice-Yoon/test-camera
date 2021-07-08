f9cdf07f88c4ca976fa71c344b42c8a1d160ef00
윤지혜 <yoonjihye@yunjihyeui-MacBook-Pro.local>
2021-07-08 14:57:08 +0900

    test size: width(144-480), height(192-640)

--- app.js
+++ app.js
@@ -16,8 +16,14 @@ const userMedia = navigator.mediaDevices.getUserMedia({
     //   min: 144,
     //   max: 480
     // },
-    width: window.screen.width,
-    height: window.screen.height/3,
+    width: {
+      min: 144,
+      max: 480
+    },
+    height: {
+      min: 192,
+      max: 640
+    },
     facingMode: 'environment' // facingMode: 'environment'(후면카메라) / 'user'(정면카메라)
   }
 })
